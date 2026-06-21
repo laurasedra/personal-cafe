9d745d8b06e8b188ea985dab1f30212b2755b72b
Laura Sedra <laurasedra@example.com>
2026-06-20 20:25:20 -0400

    fixed source for umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { UmamiAnalytics } from '@/components/UmamiAnalytics';
+import { UmamiAnalytics } from '@/app/components/UmamiAnalytics';
 
 
 const geistSans = Geist({
